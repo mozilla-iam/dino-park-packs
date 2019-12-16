d69c56fd0d6c5311feb6673cdcfc94b4b6f6c2a9
Florian Merz <flomerz@gmail.com>
2019-12-16 14:15:18 +0100

    codebuild

---
+++ terraform/codebuild/variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+  description = "DinoPark Packs - DinoParks access group service"
+  default     = "dino-park-packs"
+}
+
+variable "github_repo" {
+  default = "https://github.com/mozilla-iam/dino-park-packs"
+}
+
+variable "buildspec_file" {
+  description = "Path and name of your builspec file"
+  default     = "buildspec.yml"
+}
+
+# Find all the supported images by AWS here: 
+# https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+variable "build_image" {
+  default = "aws/codebuild/standard:1.0"
+}
+
