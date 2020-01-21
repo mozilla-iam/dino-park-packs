db213143bc4200f8f22aa08e19660a6c2489b4c0
Florian Merz <flomerz@gmail.com>
2020-01-21 15:56:54 +0100

    add terraform for test

---
+++ terraform/test/data.tf
@@ -0,0 +1,12 @@
+data "aws_caller_identity" "current" {
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"  
+
+  config = {
+    bucket = "eks-terraform-shared-state"
+    key    = "prod/us-west-2/vpc/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
