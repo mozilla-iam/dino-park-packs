72297aef22f6abcde479674bb4668d59850beede
Florian Merz <me@fiji-flo.de>
2020-06-02 13:17:38 +0200

    add terraform for prod db

--- terraform/prod/iam.tf
+++ terraform/prod/iam.tf
@@ -36,3 +36,38 @@ data "aws_iam_policy_document" "dino_park_packs" {
     ]
   }
 }
+
+resource "aws_iam_role_policy" "dino_park_packs_ssm_access" {
+  name = "dino-park-packs-ssm-access-${var.environment}-${var.region}"
+  role = aws_iam_role.dino_park_packs.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter"
+            ],
+            "Resource": [
+                "arn:aws:ssm:us-west-2:${data.aws_caller_identity.current.account_id}:parameter/iam/cis/production/*"
+            ],
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "arn:aws:kms:us-west-2:320464205386:key/ef00015d-739b-456d-a92f-482712af4f32"
+            ],
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+
+}
