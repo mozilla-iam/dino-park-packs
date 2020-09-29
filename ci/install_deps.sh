b7f6b709a43ac287fcc62064c364026676e0dd39
Florian Merz <me@fiji-flo.de>
2020-09-29 19:31:20 +0200

    move to do.sh

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -26,6 +26,3 @@ rustc --version
 rustup target add x86_64-unknown-linux-gnu
 HELM_INSTALL_DIR=/bin
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
-curl -L -o myke https://github.com/fiji-flo/myke/releases/download/0.9.11/myke-0.9.11-x86_64-unknown-linux-musl
-chmod +x ./myke
-mv ./myke /bin/myke
