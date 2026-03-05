b760a252d3c0102d78cf0645b2db8bf7ae6796d0
Bheesham Persaud <bhee@mozilla.com>
2026-03-05 18:08:41 -0500

    lint: Allow non_local_definitions to appease lints about diesel
    Jira: IAM-1908

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// DEBT: This is required because of diesel.
+// Prior art: https://github.com/mozilla-services/syncstorage-rs/blob/f2e18e2df9e166a66d00ecf41b89806b905b86d6/syncstorage-mysql/src/lib.rs#L1
+//
+// It's not clear to me if there's a future where we'll be able to disable this
+// lint.
+#![allow(non_local_definitions)]
+
 #[macro_use]
 extern crate diesel;
 #[macro_use]
